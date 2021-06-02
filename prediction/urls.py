8a036727162edc833df662107649b6ab92d36cf6
23subbhashit <subbhashitmukherjee@gmail.com>
2021-06-02 19:21:18 +0530

    init pages

--- prediction/urls.py
+++ prediction/urls.py
@@ -3,4 +3,5 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.main,name="main"),
+    path('result',views.Result,name="Result"),
 ]
--- prediction/views.py
+++ prediction/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request,'prediction/main.html')
+
+def Result(request):
+    return render(request,'prediction/Result.html')
