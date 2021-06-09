205d66be1b7542bb43c965474cc73b8f63a218e3
23subbhashit <subbhashitmukherjee@gmail.com>
2021-06-09 08:00:02 +0530

    Updates

--- prediction/camera.py
+++ prediction/camera.py
@@ -6,10 +6,17 @@ import urllib.request
 import numpy as np
 from django.conf import settings
 import os
+from tensorflow.keras.models import load_model
+import numpy as np
+from PIL import Image
+import datetime
 
-
+model = os.path.join(
+			settings.BASE_DIR,'model/mymodel.h5')
 face_detection_videocam = cv2.CascadeClassifier(os.path.join(
 			settings.BASE_DIR,'opencv_haarcascade_data/haarcascade_frontalface_default.xml'))
+    
+mymodel=load_model(model)
 
 class VideoCamera(object):
     def __init__(self):
@@ -22,12 +29,25 @@ class VideoCamera(object):
     
     def get_frame(self):
 
-        success, image = self.video.read()
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        success, img = self.video.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces_detected = face_detection_videocam.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
         for (x, y, w, h) in faces_detected:
-            cv2.rectangle(image, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=5)
-        frame_flip = cv2.flip(image, 1)
-        ret, jpeg = cv2.imencode('.jpg', frame_flip)
+            face = img[y:y+h, x:x+w]
+            imgp = Image.fromarray(face, 'RGB')
+            res = imgp.resize((150, 150))
+            test_image = np.expand_dims(res, axis=0)
+            pred=mymodel.predict(test_image)[0][0]
+            if pred==1:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+                cv2.putText(img,'MASK',((x+w)//2,y+h+20),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
+            else:
+                
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+                cv2.putText(img,'NO MASK',((x+w)//2,y+h+20),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3)
+            datet=str(datetime.datetime.now())
+            cv2.putText(img,datet,(400,450),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        # frame_flip = cv2.flip(img, 1)
+        ret, jpeg = cv2.imencode('.jpg', img)
         return jpeg.tobytes()
 
